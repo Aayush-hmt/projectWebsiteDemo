5031b2c3aa4bc485ce2dba4eb943abd7821bf322
aayush <aayushs.bca2023@ssism.org>
2024-12-13 22:02:47 +0530

    1 section is completed

--- script.js
+++ script.js
@@ -1,4 +1,49 @@
-// let addBrTage=document.querySelector(".add-br-tag");
+let tar = document.querySelector(".image-play");
+let video = document.querySelector(".custom-image");
+tar.addEventListener("click", () => {
+    video.setAttribute("controls", true);
 
-// let brTage = document.createElement("br");
-// addBrTage.nextSibling=`${brTage}${brTage}${brTage}${brTage}`;
+});
+
+
+let mainBox = document.querySelector(".main-hov-box");
+let box1 = document.querySelector("#counting-div-1");
+let box2 = document.querySelector("#counting-div-2");
+let box3 = document.querySelector("#counting-div-3");
+let box4 = document.querySelector("#counting-div-4");
+
+console.log(mainBox)
+console.log(box1)
+console.log(box2)
+console.log(box3)
+console.log(box4)
+let i = 0;
+mainBox.addEventListener('mouseover', () => {
+
+   
+  let id= setInterval(() => {
+
+        if (i <= 2.5) {
+            box1.innerHTML = `${i.toFixed(1)}M+`;
+        }
+        if (i <= 12) {
+            box2.innerHTML = `${i.toFixed(1)}M+`;
+
+
+        } if (i <= 5) {
+            box3.innerHTML = `${i.toFixed(1)}+`;
+
+        } if (i <= 12) {
+            box4.textContent= `${i.toFixed(1)}`;
+
+        }
+        i = i+0.1;
+     
+        
+        console.log("execetue is")
+        if(i>15){
+            clearInterval(id);
+        }
+    },100);
+});
+
